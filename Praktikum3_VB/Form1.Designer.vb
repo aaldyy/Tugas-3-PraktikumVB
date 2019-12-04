62973ca8f7fd9ae31e5de5cb921903882f9975db
aaldyy <aldirenaldialamin@Gmail.com>
2019-12-04 11:23:46 +0800

    zzzz

---
+++ Praktikum3_VB/Form1.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.textBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Ravie", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Maroon
+        Me.Label1.Location = New System.Drawing.Point(27, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(760, 37)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Aplikasi Kas (Masuk dan Keluar)"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(108, 83)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 25)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Tanggal"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(108, 129)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 25)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Jenis"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(108, 178)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 25)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Jumlah"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(108, 232)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(114, 25)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Keterangan"
+        '
+        'textBox1
+        '
+        Me.textBox1.Location = New System.Drawing.Point(250, 87)
+        Me.textBox1.Multiline = True
+        Me.textBox1.Name = "textBox1"
+        Me.textBox1.Size = New System.Drawing.Size(152, 25)
+        Me.textBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(250, 178)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(152, 25)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(250, 232)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(152, 24)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Red
+        Me.Label6.Location = New System.Drawing.Point(608, 87)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(136, 25)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Saldo Utama"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(610, 112)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(30, 17)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Rp."
+        '
+        'Label8
+        '
+        Me.Label8.Location = New System.Drawing.Point(655, 116)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(132, 23)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Label8"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(99, 285)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(645, 150)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(422, 233)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Simpan"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(613, 205)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Edit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(694, 205)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Hapus"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Masuk", "Keluar"})
+        Me.ComboBox1.Location = New System.Drawing.Point(250, 132)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(152, 21)
+        Me.ComboBox1.TabIndex = 16
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(613, 178)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(156, 20)
+        Me.TextBox4.TabIndex = 17
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(618, 158)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(68, 13)
+        Me.Label9.TabIndex = 18
+        Me.Label9.Text = "Search By Id"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(829, 504)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.textBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Black
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents textBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label9 As Label
+End Class
---
+++ Praktikum3_VB/Form1.vb
@@ -0,0 +1,131 @@
+﻿Imports System.Data.Odbc
+Public Class Form1
+
+    Public SaldoSekarang As String
+
+    Sub TampilGrid()
+        bukakoneksi()
+        DA = New OdbcDataAdapter("Select * from  tbl_tampilan", CONN)
+        DS = New DataSet
+        DA.Fill(DS, "tbl_tampilan")
+        DataGridView1.DataSource = DS.Tables("tbl_tampilan")
+        tutupkoneksi()
+
+    End Sub
+
+    Sub getSaldoUtama()
+        bukakoneksi()
+
+        DA = New OdbcDataAdapter("SELECT * from tbl_tampilan order by id desc limit 1", CONN)
+        DS = New DataSet
+        DA.Fill(DS, "tbl_tampilan")
+        Label8.Text = DS.Tables(0).Rows(0).Item(4)
+        SaldoSekarang = DS.Tables(0).Rows(0).Item(4)
+
+        tutupkoneksi()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TampilGrid()
+        getSaldoUtama()
+    End Sub
+    Sub KosongkanData()
+        textBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        If textBox1.Text = "" Or TextBox2.Text = "" Or TextBox3.Text = "" Then
+            MsgBox("Silahkan Isi Semua From")
+
+        Else
+            If ComboBox1.Text = "Masuk" Then
+                Dim saldoBaru As Integer
+                Dim saldoMasuk As Integer = CInt(TextBox2.Text)
+                Dim saldoTerakhir As Integer = CInt(SaldoSekarang)
+                saldoBaru = saldoMasuk + saldoTerakhir
+
+                bukakoneksi()
+                Dim simpan1 As String = "insert into tbl_tampilan (tanggal,jenis,jumlah,saldo_sekarang,keterangan) values('" & textBox1.Text & "','" & ComboBox1.Text & "','" & TextBox2.Text & "','" & saldoBaru & "','" & TextBox3.Text & "')"
+                CMD = New OdbcCommand(simpan1, CONN)
+                CMD.ExecuteNonQuery()
+                MsgBox("Input data Berhasil")
+                TampilGrid()
+                getSaldoUtama()
+                KosongkanData()
+                tutupkoneksi()
+
+            ElseIf ComboBox1.Text = "Keluar" Then
+                Dim saldoBaru As Integer
+                Dim saldoKeluar As Integer = CInt(TextBox2.Text)
+                Dim saldoTerakhir As Integer = CInt(SaldoSekarang)
+                saldoBaru = saldoTerakhir - saldoKeluar
+
+                bukakoneksi()
+                Dim simpan1 As String = "insert into tbl_tampilan (tanggal,jenis,jumlah,saldo_sekarang,keterangan) values('" & textBox1.Text & "','" & ComboBox1.Text & "','" & TextBox2.Text & "','" & saldoBaru & "','" & TextBox3.Text & "')"
+                CMD = New OdbcCommand(simpan1, CONN)
+                CMD.ExecuteNonQuery()
+                MsgBox("Input data Berhasil")
+                TampilGrid()
+                getSaldoUtama()
+                KosongkanData()
+                tutupkoneksi()
+
+            End If
+        End If
+    End Sub
+
+    Private Sub TextBox1_KeyPress1(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles TextBox4.KeyPress
+        textBox1.MaxLength = 6
+        If e.KeyChar = Chr(13) Then
+            bukakoneksi()
+            CMD = New OdbcCommand("Select * from tbl_tampilan where id ='" & TextBox4.Text & "'", CONN)
+            RD = CMD.ExecuteReader
+            RD.Read()
+            If Not RD.HasRows Then
+                MsgBox("No Tidak Ada, Silahkan coba lagi!")
+                textBox1.Focus()
+            Else
+                textBox1.Text = RD.Item("Tanggal")
+                ComboBox1.Text = RD.Item("Jenis")
+                TextBox2.Text = RD.Item("Jumlah")
+                TextBox3.Text = RD.Item("Keterangan")
+                TextBox4.Focus()
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByValsender As Object, ByVal e As EventArgs) Handles Button2.Click
+        bukakoneksi()
+        Dim edit As String = "update tbl_tampilan set
+        Tanggal='" & textBox1.Text & "',
+        Jenis='" & ComboBox1.Text & "',
+        Jumlah='" & TextBox2.Text & "',
+        Keterangan='" & TextBox3.Text & "'
+        where id ='" & TextBox4.Text & "'"
+
+        CMD = New OdbcCommand(edit, CONN)
+        CMD.ExecuteNonQuery()
+        MsgBox("Data Berhasil diUpdate")
+        TampilGrid()
+        KosongkanData()
+        tutupkoneksi()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button3.Click
+        If TextBox4.Text = "" Then
+            MsgBox("Silahkan Pilih Data yag akan dihapus dengan Masukan id dan Enter")
+        Else
+            If MessageBox.Show("Yakin akan dihapus..? ", "", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) Then
+                bukakoneksi()
+                Dim hapus As String = "delete from tbl_tampilan where id='" & TextBox4.Text & "'"
+                CMD = New OdbcCommand(hapus, CONN)
+                CMD.ExecuteNonQuery()
+                TampilGrid()
+                KosongkanData()
+                tutupkoneksi()
+            End If
+        End If
+    End Sub
+End Class
